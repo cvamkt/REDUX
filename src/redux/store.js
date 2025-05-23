07c3c36ff7dd6e21b9fc06017d22475d1aa3d47e
cvamkt <shivamkrthakur9771@gmail.com>
2025-05-23 11:28:23 +0530

    removed deleted files

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../Features/counter/counter'
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-})
